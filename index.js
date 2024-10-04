9864086d51117d57d6d0048c21cb82b8f7ec3f8f
SweatyCircle439 <155570562+SweatyCircle439@users.noreply.github.com>
2024-10-04 12:08:37 +0200

    orbit

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+const controls = new OrbitControls( camera, renderer.domElement );
 renderer.setSize( window.innerWidth, window.innerHeight );
 renderer.setAnimationLoop( animate );
 document.body.appendChild( renderer.domElement );
@@ -24,10 +26,12 @@ loader.load( 'model.gltf', function ( gltf ) {
 
 camera.position.z = 5;
 
+const light = new THREE.AmbientLight( 0x404040 ); // soft white light
+scene.add( light );
 function animate() {
+
+    controls.update();
 
     renderer.render( scene, camera );
 
 }
-const light = new THREE.DirectionalLight( 0xFFFFFF );
-scene.add( light );
